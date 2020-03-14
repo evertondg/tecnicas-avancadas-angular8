adfcaeee11d469e2029f91920f1538a6a34c264f
evertondg <evertondg@hotmail.com.br>
2020-03-14 12:18:54 -0300

    feat: Adicionado Modal com informações do registro cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-
-import { MaterialModule } from './shared/material/material.module';
-import { TopoComponent } from './shared/components/topo/topo.component';
-import { RodapeComponent } from './shared/components/rodape/rodape.component';
-
 import { FilmesModule } from './filmes/filmes.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './shared/material/material.module';
 
+import { AlertaComponent } from './shared/components/alerta/alerta.component';
+import { AppComponent } from './app.component';
+import { RodapeComponent } from './shared/components/rodape/rodape.component';
+import { TopoComponent } from './shared/components/topo/topo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopoComponent,
-    RodapeComponent,
-  ],
+  declarations: [AppComponent, TopoComponent, RodapeComponent, AlertaComponent],
   imports: [
-    BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FilmesModule,
+    HttpClientModule,
     LayoutModule,
     MaterialModule,
-    AppRoutingModule,
-    FilmesModule
   ],
+  entryComponents: [AlertaComponent],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidarCamposService } from '../../shared/components/campos/validar-campos.service';
+import { FilmesService } from '../../core/filmes.service';
+import { Filme } from 'src/app/shared/models/filme';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertaComponent } from '../../shared/components/alerta/alerta.component';
+import { Alerta } from 'src/app/shared/models/alerta';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -13,7 +19,10 @@ export class CadastroFilmesComponent implements OnInit {
 
   constructor(
     public validacao: ValidarCamposService,
-    private fb: FormBuilder
+    public dialog: MatDialog,
+    private fb: FormBuilder,
+    private filmesService: FilmesService,
+    private router: Router
   ) {}
 
   get f() {
@@ -48,16 +57,52 @@ export class CadastroFilmesComponent implements OnInit {
     ];
   }
 
-  salvar(): void {
+  submit(): void {
     this.cadastro.markAllAsTouched();
+    console.log(this.cadastro);
     if (this.cadastro.invalid) {
       return console.log('Formulário não é válido');
     }
 
-    alert('Sucesso!!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+    const filme = this.cadastro.getRawValue() as Filme;
+    this.salvar(filme);
   }
 
   reiniciarForm(): void {
     this.cadastro.reset();
+  }
+
+  private salvar(filme: Filme): void {
+    this.filmesService.salvar(filme).subscribe(
+      () => {
+        const config = {
+          data: {
+            btnSucesso: 'Ir para a Listagem',
+            btnCancelar: 'Cadastrar um novo Filme',
+            possuiBtnFechar: true,
+            btnColorCancelar: '#ff2673',
+          } as Alerta,
+        };
+        const dialogRef = this.dialog.open(AlertaComponent, config);
+        dialogRef.afterClosed().subscribe((opcao: boolean) => {
+          if (opcao) {
+            this.router.navigateByUrl('filmes');
+          } else {
+            this.reiniciarForm();
+          }
+        });
+      },
+      err => {
+        const config = {
+          data: {
+            titulo: 'Erro ao Salvar o registro',
+            descricao: 'Não conseguimos salvar seu registro',
+            btnSucesso: 'Ir para a Listagem',
+            btnColorCancelar: 'warn',
+          } as Alerta,
+        };
+        const dialogRef = this.dialog.open(AlertaComponent, config);
+      }
+    );
   }
 }
